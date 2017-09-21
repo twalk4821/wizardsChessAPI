e66f48a91d5047276edc1481f0d5433cdda0b0fd
Tyler Walker <tyjohnwalker@gmail.com>
2017-09-20 20:35:31 -0700

    multi components with material-ui styling

--- client/src/containers/MultiContainer.js
+++ client/src/containers/MultiContainer.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import MultiplayerHeader from '../components/multiplayerHeader.js';
 
 class MultiContainer extends Component {
 	constructor(props) {
 	  super(props)
-	  this.state = {}
+	  this.state = {
+	  	roomName: "Create Room"
+	  }
 	  //bind functions
+	  this.handleChange = this.handleChange.bind(this)
 	}
 
-
+	handleChange(e, color) {
+		console.log(e.target.value, color)
+		this.setState({
+			roomName: e.target.value
+		})
+	}
 	render(){
 	  return(
 	  	<div>
@@ -15,6 +24,17 @@ class MultiContainer extends Component {
 	  	    <h2>Wizards Chess</h2>
 	  	  </div>
 	      <h1>Multi Player</h1>
+	      <MultiplayerHeader 
+	        type='Host'
+	        value={this.state.roomName}
+	        message='Create a gameroom and challenge opponents in public and private matches'
+	        handleChange={this.handleChange}
+	      />
+	      <MultiplayerHeader 
+	        type='Join'
+	        value='Enter Room'
+	        message='Join a currently open gameroom to challenge or observe'
+	      />
 	    </div>
 	  )
 	}
