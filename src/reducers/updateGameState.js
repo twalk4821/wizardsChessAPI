2a6fdd59cfd148563771d228b26f2b4d776997dc
twalk4821 <tyjohnwalker@gmail.com>
2018-01-07 23:34:15 -0800

    updateGameState defaults to array rather than null

--- src/reducers/updateGameState.js
+++ src/reducers/updateGameState.js
@@ -1,6 +1,6 @@
 import { UPDATE_GAME_STATE } from '../actions';
 
-const updateGameState = (state = {board: null, turn: "white", lastMove: null, turnCount: 1, playing: false}, action) => {
+const updateGameState = (state = {board: null, turn: "white", lastMove: [], turnCount: 1, playing: false}, action) => {
 	switch (action.type) {
 		case UPDATE_GAME_STATE:
 			return action.payload
